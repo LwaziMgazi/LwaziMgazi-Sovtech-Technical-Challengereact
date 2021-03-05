ae18c4587d9bd83e54e4c6734e5ef3ec74593f99
LwaziMgazi <34287957+LwaziMgazi@users.noreply.github.com>
2021-03-05 09:46:37 -0800

    Delete db.js

--- serverside/db.js
+++
@@ -1,9 +0,0 @@
-const {DataStore}= require('notarealdb');
-
-const store = new DataStore('./data');
-
-module.exports={
-    company: store.collection('company'),
-    jobs: store.collection('jobs'),
-    users: store.collection('users')
-}
